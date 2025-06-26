f0af7c61cffef1840a301c60dc717c89f9872308
nawaphonwongyala <s6806022511021@email.kmutnb.ac.th>
2025-06-26 16:37:21 +0700

    FirstCommit

---
+++ Lecture-01/testprint.py
@@ -0,0 +1,9 @@
+#print('Hello World')
+#print('Nawaphon Wongyala')
+#print("I'm hungry")
+#print("I'm good and \"I will do my best\"")
+print('/\\')
+#print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
