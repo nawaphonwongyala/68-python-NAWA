ce8288355ea0622bbfccb81e97fd8d476d9f5c9c
nawawphonwongyala <s6806022511021@email.kmutnb.ac.th>
2025-07-17 18:33:06 +0700

    WFH04

--- Lecture-04/1-100.py
+++ Lecture-04/1-100.py
@@ -1,3 +1,11 @@
 r = int(input('How many rows?: '))
-for num in range (1,101):
-    print(num)
+total = 100
+c = total // r
+num=1
+for i in range(r):
+    for j in range(c):
+        if num > 100:
+            break
+        print(f"{num:3}", end="")
+        num += 1
+    print()
--- Lecture-04/rows_columns.py
+++ Lecture-04/rows_columns.py
@@ -1,5 +1,6 @@
 rows = int(input('How many rows?: '))
 columns = int(input('How many columns?: '))
-for num in range(rows):
-    num = ('*' * columns)
-    print(num)
+for i in range(rows):
+    for j in range(columns):
+        print('*', end="")
+    print()
